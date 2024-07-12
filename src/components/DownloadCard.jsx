7a99e3132fd2012de1ead7292cd50e377e2024c0
Vimal Vs <vimalchan24@gmail.com>
2024-07-12 20:23:59 +0530

    font & color changes

--- src/components/DownloadCard.jsx
+++ src/components/DownloadCard.jsx
@@ -3,24 +3,19 @@ import Circle from '../assets/blackCircle.svg'
 import Arrow from '../assets/arrow_RU.svg'
 import Link from "next/link"
 
-export default function DownloadCard( {name, bgColor, icon, link} ) {
-  console.log("++++++++++++++++++++++++++++++++++++++++", bgColor)
+export default function DownloadCard({ name, bgColor, icon, link }) {
   return (
     <div className="py-4 flex justify-center">
-        <div className={`block p-6 max-w-sm rounded-3xl text-white columns-2 border-b-4 border-black`} style={{ backgroundColor : bgColor}}>
-            <h1 className="text-xl pb-16 pr-11 pl-1">{name}</h1>
-            <div>
-              <Link href={link} type="button" className="absolute ml-9 mt-1">Click Here</Link>
-              
-              <Image src={Circle} width={30} height={30} className="absolute"/>
-              <Image src={Arrow} width={17} height={15} className="absolute m-2"/>
-            </div>
-            <Image src={icon} width={150} height={150} className="pr-13"/>
+      <div className={`block p-6 max-w-sm rounded-3xl text-white columns-2 border-b-4 border-black`} style={{ backgroundColor: bgColor }}>
+        <h1 className="text-xl pb-16 pr-11 pl-1">{name}</h1>
+        <div>
+          <Link href={link} type="button" className="absolute ml-9 mt-1">Click Here</Link>
+
+          <Image src={Circle} width={30} height={30} className="absolute" />
+          <Image src={Arrow} width={17} height={15} className="absolute m-2" />
         </div>
+        <Image src={icon} width={150} height={150} className="pr-13" />
+      </div>
     </div>
   )
 }
-
-
-// className="flex text-sm m-1 justify-center"
-// className="py-2"
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,19 +1,17 @@
 import Image from 'next/image'
 import quote from '../assets/inverted_quote.svg'
 
-export default function TestimonialCard( {name, designation, content} ) {
+export default function TestimonialCard({ name, designation, content }) {
     return (
         <div className="py-4 flex justify-center">
-       
-            <div class="block px-4 py-2 max-w-md bg-[#8280F2] rounded-2xl border ">
-            <Image src={quote} width={50} height={50} className="pb-4" />
-                <h1 class="mb-2 font-sm text-white">
+            <div className="block px-4 py-2 max-w-md bg-purple rounded-2xl border">
+                <Image src={quote} width={50} height={50} className="pb-4" />
+                <h1 className="mb-2 font-sm text-white">
                     {content}
                 </h1>
                 <p className=" font-md text-white">{name}</p>
                 <p className=" font-normal text-yellow-400 pb-4">{designation}</p>
             </div>
-
         </div>
     )
 }
