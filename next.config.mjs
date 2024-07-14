2ec2cd72a08b2ab69de0308011508e81751b14b9
Vimal Vs <vimalchan24@gmail.com>
2024-07-14 13:01:03 +0530

    webpack for playback

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+        domains: ['images.unsplash.com'],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.(mp4|webm|ogg|swf|ogv)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: '/_next/static/videos/',
+                    outputPath: 'static/videos/',
+                    name: '[name].[ext]',
+                    esModule: false,
+                },
+            },
+        });
+
+        return config;
+    },
 };
 
-export default nextConfig;
+export default nextConfig;  
