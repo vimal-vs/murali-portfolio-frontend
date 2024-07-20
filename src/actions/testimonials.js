349e4a5c4e7d4058b8f2500c4aaef8dc0defa1f9
Vimal Vs <vimalchan24@gmail.com>
2024-07-20 12:24:17 +0530

    endpoint changed

--- src/actions/testimonials.js
+++ src/actions/testimonials.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllTestimonials = async () => {
     try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/testimonials`);
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/testimonials/all`);
         return response.data
     } catch (error) {
         console.error('Error fetching blogs:', error);
