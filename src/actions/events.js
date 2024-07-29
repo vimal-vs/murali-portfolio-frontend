ad939b9d05335659a7205c80c386562319264aac
Vimal Vs <vimalchan24@gmail.com>
2024-07-29 11:54:42 +0530

    limit-query added

--- src/actions/events.js
+++ src/actions/events.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllEvents = async () => {
     try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/events/all`);
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/events/all?limit=3`);
         return response.data
     } catch (error) {
         console.error('Error fetching blogs:', error);
--- src/actions/podcasts.js
+++ src/actions/podcasts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllPodcasts = async () => {
     try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/podcasts/all`);
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/podcasts/all?limit=6`);
         return response.data
     } catch (error) {
         console.error('Error fetching blogs:', error);
